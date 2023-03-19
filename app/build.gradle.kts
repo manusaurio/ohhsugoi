f79747a7cd973559d976bcc95498c0cfac93d6a5
Manuel Ignacio Pérez Alcolea <perezalcoleam@gmail.com>
2023-03-18 22:24:33 -0300

    Add SQL with sqldelight, along table definitions and some generated functions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 
 dependencies {
     implementation("dev.kord", "kord-core", "0.8.0-M17")
+    implementation("app.cash.sqldelight", "sqlite-driver", "2.0.0-alpha05")
+    implementation("app.cash.sqldelight", "primitive-adapters", "2.0.0-alpha05")
 }
 
 application {
@@ -18,6 +20,14 @@ application {
     // this won't actually help that much but imma gonna have
     // this running on an old raspberry pi you know?
     applicationDefaultJvmArgs = listOf("-Xms256m", "-Xmx512m")
+}
+
+sqldelight {
+    databases {
+        create("Database") {
+            packageName.set("ar.pelotude")
+        }
+    }
 }
 
 // not a fan of fat jars
