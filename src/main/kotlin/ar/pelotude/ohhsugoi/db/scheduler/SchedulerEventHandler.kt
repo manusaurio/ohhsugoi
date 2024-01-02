54fd13c18c0e60a9dabf797703010f1d7413b0df
Manuel Ignacio Pérez Alcolea <perezalcoleam@gmail.com>
2024-01-02 14:04:45 -0300

    Fix scheduler events API

--- src/main/kotlin/ar/pelotude/ohhsugoi/db/scheduler/SchedulerEventHandler.kt
+++ src/main/kotlin/ar/pelotude/ohhsugoi/db/scheduler/SchedulerEventHandler.kt
@@ -1,9 +1,9 @@
 package ar.pelotude.ohhsugoi.db.scheduler
 
-interface SchedulerEventHandler<T> {
+interface SchedulerEventHandler<T : Any> {
     fun handle(e: ScheduleEvent<T>)
 }
 
-sealed class ScheduleEvent<T>(val post: ScheduledPostMetadataImpl<T>)
-class Success<T>(post: ScheduledPostMetadataImpl<T>): ScheduleEvent<T>(post)
-class Failure<T>(post: ScheduledPostMetadataImpl<T>, val reason: String): ScheduleEvent<T>(post)
+sealed class ScheduleEvent<T : Any>(val post: StoredPost<T, out SchedulablePost>)
+class Success<T : Any>(post: StoredPost<T, out SchedulablePost>): ScheduleEvent<T>(post)
+class Failure<T : Any>(post: StoredPost<T, out SchedulablePost>, val reason: String): ScheduleEvent<T>(post)
